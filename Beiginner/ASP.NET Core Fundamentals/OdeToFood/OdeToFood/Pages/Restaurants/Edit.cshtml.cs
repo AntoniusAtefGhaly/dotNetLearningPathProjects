9936fe419917f36b5a9e701621706eb84b1fd8b2
Antonius Atef Ghaly <antonius.a.ghaly@gmail.com>
2022-03-26 23:38:09 +0200

    tempdata

---
+++ Beiginner/ASP.NET Core Fundamentals/OdeToFood/OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OdeToFood.Core;
+using OdeToFood.Data;
+using System.Collections.Generic;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class EditModel : PageModel
+    {
+        private readonly IrestaurantData restaurantData;
+        private readonly IHtmlHelper htmlHelper;
+
+        [BindProperty]
+        public Restaurant Restaurant {get; set;}
+
+        public IEnumerable<SelectListItem> Ocusins {get; set;}
+
+        public EditModel(IrestaurantData restaurantData,IHtmlHelper htmlHelper)
+        {
+            this.restaurantData = restaurantData;
+            this.htmlHelper = htmlHelper;
+            Ocusins = this.htmlHelper.GetEnumSelectList<CuisineType>();
+        }
+        public ActionResult OnGet(int? id)
+        {
+            if (id.HasValue)
+            {
+                Restaurant = restaurantData.GetById(id.HasValue?id.Value:0);
+            }
+            else
+            {
+                Restaurant=new Restaurant();
+            }
+            if (Restaurant == null)
+                return RedirectToPage("./NotFound");
+            return Page();
+        }
+
+        public ActionResult OnPost( )
+        {
+            if (ModelState.IsValid)
+            {
+                var res  = restaurantData.GetById(Restaurant.Id);
+                if (res == null)
+                    restaurantData.Add(Restaurant);
+                else
+                restaurantData.Update(Restaurant);
+                TempData["Message"] = "succful saved";
+                return RedirectToPage($"./Details",new 
+                {
+                     id= Restaurant.Id 
+                });
+
+            }
+            return Page();
+        }
+    }
+}
